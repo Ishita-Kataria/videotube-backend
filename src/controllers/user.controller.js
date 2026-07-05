1f7d3ad341f3dffaec726b3f79fdd81c4b23cc27
Ishita Kataria <justmeishita11@gmail.com>
2026-07-05 18:16:45 +0530

    Add auth middleware and logout controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,5 +118,25 @@ const loginUser = asyncHandler(async (req, res) => {
             )
         );
 });
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: { refreshToken: 1 }
+        },
+        { new: true }
+    );
 
-export { registerUser, loginUser };
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "Logout successful!"));
+});
+
+export { registerUser, loginUser, logoutUser };
