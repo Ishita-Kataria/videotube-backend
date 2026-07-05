5b40a33c0866db81ce41873b85766b4dbd3bd98e
Ishita Kataria <justmeishita11@gmail.com>
2026-07-05 14:16:11 +0530

    Add healthcheck route

--- src/app.js
+++ src/app.js
@@ -12,5 +12,8 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
